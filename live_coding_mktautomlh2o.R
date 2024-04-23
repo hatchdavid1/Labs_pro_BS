38276fd4bd9563960a970e299921bc8100fb0813
David Hatch <51379112+hatchdavid1@users.noreply.github.com>
2024-04-23 12:51:23 -0600

    Add files via upload
    Adding the rest of the code for this project

--- live_coding_mktautomlh2o.R
+++ live_coding_mktautomlh2o.R
@@ -27,3 +27,61 @@ h2o.init()
 data_joined_tbl  <- data_joined_tbl %>%
     mutate_if(is.character, as.factor)
 
+# Converting the df to tan h2oFrame
+train  <- as.h2o(data_joined_tbl)
+
+# Quick h2oFrame view
+h2o.describe(train)
+
+# Specifying the dependant feature
+y  <- "TERM_DEPOSIT" 
+
+# Create a new df excluding the dependant variable
+x  <- setdiff(names(train), c(y, 'ID'))
+    
+# Executing an automl run for 15 mdodels
+aml  <- h2o.automl(
+    y= y,  
+    x = x, 
+    training_frame = train, 
+    project_name = 'term_deposit', 
+    max_models = 15, 
+    seed = 42)
+
+# Getting the result leaderboard for the 15 models 
+lb  <- aml@leaderboard
+
+# Printing the general result from leaderboard
+print(lb)
+
+# Printing just the results for each model 
+print(lb, n = nrow(lb))
+
+# Exploring the results
+# Getting the ids for each model from the autmoml
+model_ids <- as.data.frame(aml@leaderboard$model_id)[,1]
+
+# Getting all models as one stacked ensemble model
+se <- h2o.getModel(grep("StackedEnsemble_AllModels", model_ids, value = TRUE)[1])
+
+# Getting the metalearner which is the stacked ensembled called from h2o
+metalearner <- h2o.getModel(se@model$metalearner$name)
+
+# Showing the feature importance for the metalearner or stacked ensemnble algorithm
+h2o.varimp(metalearner)
+
+# Plotting the base learner contributions to the ensemble.
+h2o.varimp_plot(metalearner)
+
+
+# 4.5 Variable Importance ----
+
+# Getting the Xgboost which is the second best in order to compare
+xgb <- h2o.getModel(grep("XGBoost", model_ids, value = TRUE)[1])
+
+# Showing the feature importance for the top XGBoost model
+h2o.varimp(xgb)
+
+# Plotting the base learner contributions to the ensemble.
+h2o.varimp_plot(xgb)
+    
