349ebaafd118906ccf6b10f32bbaaf46a51004b6
David Hatch <51379112+hatchdavid1@users.noreply.github.com>
2024-04-26 10:42:26 -0600

    Add files via upload
    Finishing the code and found out that in order to run it you must consider tidyr .8.3 until a newer version of anomilize package

--- anomalize_package_lab.R
+++ anomalize_package_lab.R
@@ -5,17 +5,49 @@ library(tidyverse)
 library(tidyquant)
 library(lubridate)
 library(rsample)
+library(tibbletime)
 library(anomalize)
 
 #### General Structure #### 
+# Loading data  (Kaggle competition)
 websites_raw_tbl  <- vroom::vroom('/Users/davidhatch/Documents/labs_pro/lab_anomalize_package/lab_12_r_programming_rlang/data/train_1.csv', delim = ',')
 
-websites_raw_tbl  <- websites_raw_tbl %>% rowid_to_column(var = 'id')
+websites_raw_tbl <- websites_raw_tbl %>%
+  rowid_to_column(var = "id")
 
 set.seed(123)
 websites_sample_tbl <- websites_raw_tbl %>%
-  sample_n(size = 9) %>% 
-  gather(key = 'date', value = 'visits', -id, -Page) %>%
+  sample_n(size = 9) %>%
+  gather(key = "date", value = "visits", -id, -Page) %>%
   replace_na(replace = list(visits = 0)) %>%
   mutate(date = ymd(date))
 
+websites_sample_tbl %>%
+  ggplot(aes(date, visits, color = Page)) +
+  geom_point(alpha = 0.5) +
+  facet_wrap(~ Page, scales = "free_y") +
+  theme_tq() + 
+  scale_color_viridis_d() +
+  theme(legend.position = "none") +
+  expand_limits(y = 0)
+
+#### Anomaly Detection #### 
+
+websites_sample_tbl %>%
+  
+  filter(Page %>% str_detect("Kit_Harington")) %>%
+  
+  time_decompose(target = visits, method = "stl") %>%
+  anomalize(target = remainder, method = "iqr", alpha = 0.3) %>%
+  time_recompose() %>%
+  
+  plot_anomalies(time_recomposed = TRUE)
+
+websites_sample_tbl %>%
+  
+  group_by(Page) %>%
+  
+  time_decompose(target = visits, method = "stl") %>%
+  anomalize(target = remainder, method = "iqr", alpha = 0.05) %>%
+  time_recompose() %>%
+  plot_anomalies(ncol = 3, time_recomposed = TRUE)
