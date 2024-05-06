f1fb435a141b36669dabe7bb219188e313066f8f
David Hatch <51379112+hatchdavid1@users.noreply.github.com>
2024-05-06 11:01:14 -0600

    Add files via upload
    Performance analysis components

--- potfolio_analysis_n_opt.R
+++ potfolio_analysis_n_opt.R
@@ -24,8 +24,9 @@ returns_m_components_tbl  <- c('BAC', 'CAT', 'CPNG', 'DIS', 'ECL', 'FICO', 'KOF'
   group_by(symbol) %>%
   tq_transmute(select = adjusted,
                mutate_fun = periodReturn, period = 'monthly',
-               col_rename = 'ret', type = 'log') %>%
+               col_rename = 'monthly.returns', type = 'log') %>%
   ungroup()
+returns_m_components_tbl
 #### Original version 
 # returns_m_components_tbl <- c("AAPL", "GOOG", "NFLX") %>%
 #   tq_get(get  = "stock.prices",
@@ -37,7 +38,58 @@ returns_m_components_tbl  <- c('BAC', 'CAT', 'CPNG', 'DIS', 'ECL', 'FICO', 'KOF'
 #                period     = "monthly") %>%
 #   ungroup()
 
+#### Benchmark_tbl #### 
+returns_m_benchmark_tbl <- "XLK" %>%
+  tq_get(get  = "stock.prices",
+         from = start,
+         to   = end) %>%
+  tq_transmute(select     = adjusted, 
+               mutate_fun = periodReturn, 
+               period     = "monthly", type = 'log') %>%
+  add_column(symbol = "XLK", .before = 1)
 
-returns_m_components_tbl
+returns_m_benchmark_tbl
+
+
+#### Performance analysis components #### 
+returns_m_tbl <- returns_m_components_tbl %>%
+  bind_rows(returns_m_benchmark_tbl)
+
+returns_m_tbl %>%
+  group_by(symbol) %>%
+  tq_performance(Ra = monthly.returns, 
+                 performance_fun = SharpeRatio.annualized, 
+                 scale = 12, 
+                 Rf    = 0) %>%
+  ungroup() %>%
+  mutate(symbol = as_factor(symbol)) %>%
+  
+  ggplot(aes(symbol, `AnnualizedSharpeRatio(Rf=0%)`)) +
+  geom_col(fill = "#2c3e50") +
+  geom_text(aes(label = `AnnualizedSharpeRatio(Rf=0%)` %>% round(2)), 
+            color = "white", nudge_y = -0.05) +
+  theme_tq() +
+  labs(
+    title = "Sharpe Ratio: Stocks vs Benchmark",
+    x = "", y = "Annualized Sharpe Ratio (Rf = 0%)"
+  )
+
+#### Portolio Aggregation  ####
+
+wts_tbl  <- returns_m_components_tbl %>%
+  distinct(symbol) %>%
+  mutate(weights = c(0.083, 0.083, 0.083, 0.083, 
+                     0.083, 0.0833, 0.083, 0.083, 0.083, 0.083, 0.083, 0.083))
+
+wts_tbl
+
+returns_m_portfolio_tbl <- returns_m_components_tbl %>%
+  tq_portfolio(symbol, RA, 
+               weights = wts_tbl,
+               rebalance_on = "quarters",
+               col_rename   = "monthly.returns"
+  )
+
+returns_m_portfolio_tbl
 
 
