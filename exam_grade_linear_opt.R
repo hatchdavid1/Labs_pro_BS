623a85b9467ed746e83c97b871faac4a076ad36c
David Hatch <51379112+hatchdavid1@users.noreply.github.com>
2024-05-14 19:24:31 -0600

    Add files via upload
    Setting everything for grade opt

---
+++ exam_grade_linear_opt.R
@@ -0,0 +1,35 @@
+#### 
+### Loading main libraries #### 
+# Solver backend 
+library(ROI)
+library(ROI.plugin.glpk)
+
+# Tidy Linear Programming 
+library(ompr)
+library(ompr.roi)
+
+# Core
+library(tidyverse)
+
+# Timing 
+library(tictoc)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
