69c454eca2b8dcd8336dd1db1d3cd2fe718faec9
David Hatch <51379112+hatchdavid1@users.noreply.github.com>
2024-05-15 11:23:29 -0600

    Add files via upload
    Non linear portfolio Optimization

---
+++ nlp_opt_portfolio.R
@@ -0,0 +1,15 @@
+#### Portoflio Optimization - Nonlinear Programming #### 
+#### Loading Main Libraries #### 
+# Optimization solver 
+library(ROI)
+library(ROI.plugin.alabama)
+# Finance 
+library(tidyquant)
+# Visualization 
+library(plotly)
+# Core 
+library(tidyverse)
+# Timing 
+library(tictoc)
+
+
