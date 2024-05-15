15ce8fe8c4b255296e742bd657b074d060fdd23c
David Hatch <51379112+hatchdavid1@users.noreply.github.com>
2024-05-15 11:38:11 -0600

    Add files via upload
    Getting the data for each selected asset

--- nlp_opt_portfolio.R
+++ nlp_opt_portfolio.R
@@ -13,3 +13,36 @@ library(tidyverse)
 library(tictoc)
 
 
+#### Data ####  BBY  CCI  EBAY  JOE  ORCL  VLTO
+assets <- c ('BBY'  ,'CCI'  ,'EBAY'  ,'JOE'  ,'ORCL'  ,'VLTO') %>% sort()
+
+stock_prices_tbl  <- tq_get(assets, from = '2022-01-01', to = '2024-01-01')
+stock_return_tbl  <- stock_prices_tbl %>%
+    select(symbol, date, adjusted) %>% 
+    group_by(symbol) %>% 
+    tq_transmute(adjusted, mutate_fun = periodReturn, period = 'yearly', col_rename = 'returns')
+
+returns_matrix_tbl  <- stock_return_tbl %>%
+    spread(symbol, returns) %>% 
+    select(assets)
+
+returns_matrix_tbl
+
+
+#### Optimization #### 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
