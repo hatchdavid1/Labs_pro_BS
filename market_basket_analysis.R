edefb01785fd464c3d26abfda797619f988d3590
David Hatch <51379112+hatchdavid1@users.noreply.github.com>
2024-05-21 10:30:15 -0600

    Add files via upload
    Adding data, calculating purchase frequency and plotting it

--- market_basket_analysis.R
+++ market_basket_analysis.R
@@ -28,3 +28,48 @@ departmentes_tbl  <- vroom("/Users/davidhatch/Documents/labs_pro/lab_market_bask
 departmentes_tbl
 
 
+#### Data Understanding #### 
+orders_combined_tbl  <- orders_products_tbl %>% 
+    left_join(orders_tbl) %>% 
+    left_join(products_tbl) %>% 
+    left_join(aisles_tbl) %>% 
+    left_join(departmentes_tbl) %>% 
+    select(eval_set, user_id, 
+           contains("order"), contains("product"), 
+           contains("aisle"), contains("department"), everything())
+
+orders_combined_tbl %>% glimpse()
+
+#### Which products are purchased most frequently 
+item_frequency_tbl  <- orders_combined_tbl %>% 
+    count(product_name, product_id, aisle, department) %>% 
+    arrange(desc(n)) %>% 
+    mutate(
+        pct = n / sum(n), 
+        cumulative_pct = cumsum(pct), 
+        popular_product = ifelse(cumulative_pct  <= .5, "Yes", "No")
+    ) %>% 
+    rowid_to_column(var = "rank") %>% 
+    mutate(label_text = str_glue("Rank: {rank}
+                                 Product: {product_name}
+                                 ProductID: {product_id}
+                                 Aisle: {aisle}
+                                 Department: {department}
+                                 Count: {n}
+                                 Pct: {scales::percent(pct)}
+                                 Cumulative Pct: {scales::percent(cumulative_pct)}"))
+
+item_frequency_tbl
+
+g  <- item_frequency_tbl %>% 
+    slice(1:5000) %>% 
+    ggplot(aes(rank, n)) + 
+    geom_point(aes(size = n, color = popular_product, text = label_text), alpha = .2) + 
+    theme_tq() +
+    scale_color_tq() + 
+    theme(legend.direction = "vertical", 
+          legend.position = "right") + 
+    labs(title = " Item Frequency", 
+         subtitle = "Top Items Account for Majority of Purchases")
+
+ggplotly(g, tooltip = 'text')
