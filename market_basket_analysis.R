5dcec07bb43503533f141d3b8c53cfb4ad5ca173
David Hatch <51379112+hatchdavid1@users.noreply.github.com>
2024-05-21 10:18:19 -0600

    Add files via upload
    Loading Data

--- market_basket_analysis.R
+++ market_basket_analysis.R
@@ -10,3 +10,21 @@ library(plotly)
 library(recommenderlab)
 library(arules)
 library(arulesViz)
+
+#### Data #### 
+orders_products_tbl  <- vroom("/Users/davidhatch/Documents/labs_pro/lab_market_basket/learning_lab_11_market_basket_analysis/00_Data/order_products__train.csv", delim = ",")
+orders_products_tbl
+
+orders_tbl  <- vroom("/Users/davidhatch/Documents/labs_pro/lab_market_basket/learning_lab_11_market_basket_analysis/00_Data/orders.csv", delim = ",")
+orders_tbl
+
+products_tbl  <- vroom("/Users/davidhatch/Documents/labs_pro/lab_market_basket/learning_lab_11_market_basket_analysis/00_Data/products.csv", delim = ",")
+products_tbl
+
+aisles_tbl  <- vroom("/Users/davidhatch/Documents/labs_pro/lab_market_basket/learning_lab_11_market_basket_analysis/00_Data/aisles.csv", delim = ",")
+aisles_tbl
+
+departmentes_tbl  <- vroom("/Users/davidhatch/Documents/labs_pro/lab_market_basket/learning_lab_11_market_basket_analysis/00_Data/departments.csv", delim = ",")
+departmentes_tbl
+
+
